f244152b51a88275753e5a14f113a4f12aa79fa2
Sunny <songseu@outlook.com>
2020-10-06 21:19:14 +0800

    提交代码

--- zvt_tushare/recorders/tushare_kdata_recoder.py
+++ zvt_tushare/recorders/tushare_kdata_recoder.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-'''=================================================
-@Project :zvt-tushare
-@FileName:tushare_kdata_recoder.py
-@Python  :Python 3.5+
-@Author  :Sunny
-@Time    :2020/8/21/0021 11:19
-@Desc    :
-=================================================='''
-
 import tushare as ts
 import pandas as pd
 
@@ -19,13 +10,41 @@ from zvt.contract.api import df_to_db
 from zvt.contract.recorder import FixedCycleDataRecorder
 from zvt.domain import register_schema, declarative_base
 
-from zvt_tm.domain import Stock1dKdata
-from zvt_tm.recorders.baostock.common import to_bs_trading_level, to_bs_entity_id
 from zvt.utils.pd_utils import pd_is_not_null
 from zvt.utils.time_utils import to_time_str, TIME_FORMAT_DAY, TIME_FORMAT_ISO8601
 
 class TuKdataRecoder(FixedCycleDataRecorder):
-    pass
+    provider = 'tushare'
+
+    entity_provider = 'tushare'
+    entity_schema = Stock
+
+    # 将Provider注册到data_schema
+    data_schema = StockKdataCommon
+
+    def __init__(self,
+                exchanges=['sh', 'sz'],
+                entity_id=None,
+                codes=None,
+                batch_size=10,
+                force_update=True,
+                sleeping_time=1,
+                default_size=2000,
+                real_time=False,
+                fix_duplicate_way='ignore',
+                start_timestamp=None,
+                end_timestamp=None,
+                level=IntervalLevel.LEVEL_1DAY,
+                kdata_use_begin_time=True,
+                close_hour=None,
+                close_minute=None,
+                one_day_trading_minutes=24 * 60) -> None:
+        level = IntervalLevel(level)
+        self.data_schema = get_kdata_schema(entity_type='stock', level=level)
+
+
+
+__all__ = ['TuKdataRecoder']
 
 if __name__ == "__main__":
     pass
